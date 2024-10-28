70e2b0993a67fd2e2a019cd4bd6c231e9e213c0c
Agus <averafaus05@gmail.com>
2024-10-29 00:29:52 +0100

    added ctions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
+const LIVE_URL = 'https://agustiinveraa.github.io';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: LIVE_URL
+});
